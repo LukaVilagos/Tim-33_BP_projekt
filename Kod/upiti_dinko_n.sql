17640b7be02acc02b954e282d57f8aa8ba1c0f5f
Luka Vilagoš <luka.vilagos@gmail.com>
2024-05-29 21:33:01 +0200

    Updated upiti_dinko and upiti_patirica

--- Kod/upiti_dinko_n.sql
+++ Kod/upiti_dinko_n.sql
@@ -1,12 +1,8 @@
 USE Sustav_za_rezervaciju_cuvara_ljubimaca;
 
-select * from usluga;
-select * from cuvar;
-select * from aktivnost;
-select * from cuvar_aktivnost;
+-- Upiti
+-- 1. Upit: Izračunava koliko je tko usluga napravio od čuvara, od svakog čuvara izračunava ukupno zaradu te koliko prosječno uzima po usluzi.
 
-
--- 1. Izračunava koliko je tko usluga napravio od čuvara, od svakog čuvara izračunava ukupno zaradu te koliko prosječno uzima po usluzi. 
 SELECT cuvar.id , cuvar.ime,cuvar.prezime,
     COUNT(usluga.id) AS broj_usluga,
     SUM(usluga.cijena) AS ukupna_zarada,
@@ -18,7 +14,8 @@ HAVING COUNT(usluga.id) > 0
 ORDER BY prosjecna_cijena DESC;
 
 
--- 2. Izračunava koliko je svaki čuvar mora raditi svoje aktivnosti(u minutama).
+-- 2. Upit: Izračunava koliko je svaki čuvar mora raditi svoje aktivnosti(u minutama).
+
 SELECT cuvar.id, cuvar.ime, cuvar.prezime,
     ROUND(SUM(TIME_TO_SEC(aktivnost.trajanje) / 60), 2) AS ukupno_trajanje_min
 FROM cuvar
@@ -27,7 +24,8 @@ INNER JOIN aktivnost ON cuvar_aktivnost.aktivnost_id = aktivnost.id
 GROUP BY cuvar.id, cuvar.ime, cuvar.prezime
 ORDER BY ukupno_trajanje_min DESC;
 
--- 3. Izračunava prosjek cijena svih usluga u gradu Zagrebu i vrača usluge koje su iznad tog prosijeka.
+-- 3. Upit: Izračunava prosjek cijena svih usluga u gradu Zagrebu i vrača usluge koje su iznad tog prosijeka.
+
 SELECT DISTINCT usluga.naziv, usluga.opis
 FROM usluga
 INNER JOIN cuvar ON usluga.cuvar_id = cuvar.id
@@ -43,8 +41,8 @@ AND usluga.cijena > (
     WHERE cuvar.grad = 'Zagreb'
 );
 
+-- 4. Upit: Izdvoji koja je osoba zaradila najviše novaca u svim gradovima. 
 
--- 4. Izdvoji koja je osoba zaradila najviše novaca u svim gradovima. 
 SELECT cuvar.grad, cuvar.ime, cuvar.prezime, usluga.naziv, usluga.opis, usluga.cijena
 FROM usluga
 JOIN cuvar ON usluga.cuvar_id = cuvar.id
@@ -56,10 +54,10 @@ WHERE usluga.cijena = (
     )
 ORDER BY cuvar.grad;
 
+-- Pogledi
+-- 1. Pogled: Koliko rezervacija ima svaka aktivnost.
 
--- 5. Koliko rezervacija ima svaka aktivnost.
--- drop view broj_rezervacija;
-create view broj_rezervacija as
+CREATE VIEW broj_rezervacija AS
 SELECT aktivnost.naziv AS vrsta_aktivnosti, COUNT(*) AS broj_rezervacija
 FROM cuvar
 JOIN cuvar_aktivnost ON cuvar.id = cuvar_aktivnost.cuvar_id
@@ -67,12 +65,11 @@ JOIN aktivnost ON cuvar_aktivnost.aktivnost_id = aktivnost.id
 GROUP BY aktivnost.naziv
 ORDER BY broj_rezervacija DESC;
 
--- test
-select * from broj_rezervacija;
+SELECT * FROM broj_rezervacija;
 
--- 6 Prema spolu određuje broj čuvara, prosječnu dob i koliko su ukupno zaradili.
--- drop view spolovi;
-create view spolovi as 
+-- 2. Pogled: Prema spolu određuje broj čuvara, prosječnu dob i koliko su ukupno zaradili.
+
+CREATE VIEW spolovi AS 
 SELECT
     cuvar.spol,
     COUNT(DISTINCT cuvar.id) AS broj_cuvara,
@@ -82,12 +79,11 @@ FROM cuvar
 JOIN usluga ON cuvar.id = usluga.cuvar_id
 GROUP BY cuvar.spol;
 
--- test 
-select * from spolovi;
+SELECT * FROM spolovi;
 
--- 7 Ovaj kod broji aktivnosti s različitim intenzitetima za svaki grad.
--- drop view intenzitet_po_gradu;
-create view intenzitet_po_gradu as 
+-- 3. Pogled: Ovaj kod broji aktivnosti s različitim intenzitetima za svaki grad.
+
+CREATE VIEW intenzitet_po_gradu AS 
 SELECT 
     cuvar.grad AS puni_naziv_grada,
     SUM(aktivnost.intenzitet = ' srednje') AS broj_srednjeg,
@@ -99,13 +95,12 @@ JOIN aktivnost ON cuvar_aktivnost.aktivnost_id = aktivnost.id
 GROUP BY cuvar.grad
 ORDER BY cuvar.grad;
 
--- test 
-select * from intenzitet_po_gradu;
+SELECT * FROM intenzitet_po_gradu;
 
 
--- 8 Prikaži sve aktivnosti koje traju duže od 1 sata i imaju više od 5 rezervacija.
--- drop view dugacke_popularne_rezervacije;
-create view dugacke_popularne_rezervacije as 
+-- 4. Pogled: Prikaži sve aktivnosti koje traju duže od 1 sata i imaju više od 5 rezervacija.
+
+CREATE VIEW dugacke_popularne_rezervacije AS
 SELECT * FROM aktivnost 
 WHERE trajanje > '01:00:00' 
 AND id IN (
@@ -114,8 +109,7 @@ AND id IN (
     GROUP BY aktivnost_id 
     HAVING COUNT(*) > 5);
 
--- test 
-select * from dugacke_popularne_rezervacije;
+SELECT * FROM dugacke_popularne_rezervacije;
 
 
 
---
+++ Kod/upiti_patricia_l.sql
@@ -0,0 +1,171 @@
+USE Sustav_za_rezervaciju_cuvara_ljubimaca;
+
+-- Grupa tablica: vrsta, pasmina, ljubimac, rezervacija_ljubimac
+-- Upiti:
+-- 1. Upit: Prikaži najmlađeg ljubimca za svaku pasminu
+
+SELECT 
+	p.naziv AS pasmina_naziv, 
+    ljubimac.ime AS ljubimac_ime, 
+    ljubimac.dob AS ljubimac_dob, 
+    ljubimac.boja AS ljubimac_boja, 
+    ljubimac.tezina AS ljubimac_tezina
+	FROM pasmina p
+	JOIN ljubimac ON p.id = ljubimac.pasmina_id
+	WHERE ljubimac.dob = (
+			SELECT MIN(lj.dob)
+				FROM ljubimac lj
+				WHERE lj.pasmina_id = p.id
+		)
+	ORDER BY p.naziv;
+    
+-- 2. Upit: Prikaži ljubimca sa više od jedne rezervacije
+
+SELECT 
+    l.id AS ljubimac_id,
+    l.ime AS ljubimac_ime,
+    p.naziv AS pasmina_naziv,
+    v.naziv AS vrsta_naziv,
+    COUNT(rl.rezervacija_id) AS broj_rezervacija
+	FROM ljubimac l
+	JOIN pasmina p ON l.pasmina_id = p.id
+	JOIN vrsta v ON p.vrsta_id = v.id
+	JOIN rezervacija_ljubimac rl ON l.id = rl.ljubimac_id
+	GROUP BY l.id, p.naziv, v.naziv
+	HAVING COUNT(rl.rezervacija_id) > 1
+	ORDER BY broj_rezervacija DESC, l.ime ASC;
+    
+-- 3. Upit: Prikaži najčešću boju ljubimca po vrsti
+    
+SELECT vrsta.naziv AS vrsta_naziv, ljubimac.boja, COUNT(*) AS broj_ljubimaca
+	FROM vrsta
+	JOIN pasmina ON vrsta.id = pasmina.vrsta_id
+	JOIN ljubimac ON pasmina.id = ljubimac.pasmina_id
+	GROUP BY vrsta.id, ljubimac.boja
+	HAVING COUNT(*) = (
+			SELECT MAX(boja_count)
+			FROM (
+				SELECT COUNT(*) AS boja_count
+					FROM pasmina AS p2
+					JOIN ljubimac AS l2 ON p2.id = l2.pasmina_id
+					WHERE p2.vrsta_id = vrsta.id
+					GROUP BY l2.boja
+			) AS broj_boja
+		)
+	ORDER BY vrsta.naziv;
+    
+-- 4. Upit: Prikaži najčešću pasminu ljubimca   
+    
+SELECT p.id AS id_pasmina, p.naziv AS pasmina_naziv, COUNT(*) AS broj_ljubimaca
+	FROM ljubimac l
+	JOIN pasmina p ON l.pasmina_id = p.id
+	GROUP BY p.id, p.naziv
+	ORDER BY broj_ljubimaca DESC;
+
+-- Pogledi:
+-- 1. Pogled: Detaljan prikaz ljubimca:
+
+CREATE VIEW detaljan_prikaz_ljubimac AS
+	SELECT 
+		l.id AS ljubimac_id,
+		l.ime AS ljubimac_ime,
+		l.dob AS ljubimac_dob,
+		l.spol AS ljubimac_spol,
+		l.boja AS ljubimac_boja,
+		l.tezina AS ljubimac_tezina,
+		l.zdravstveni_problemi AS ljubimac_zdravstveni_problemi,
+		l.veterinarski_podaci AS ljubimac_veterinarski_podaci,
+		l.posebne_potrebe AS ljubimac_posebne_potrebe,
+		l.socijalnost AS ljubimac_socijalnost,
+		l.akitvnosti AS ljubimac_aktivnosti,
+		p.naziv AS pasmina_naziv,
+		v.naziv AS vrsta_naziv,
+		(
+			SELECT COUNT(rl.rezervacija_id)
+				FROM rezervacija_ljubimac rl
+				WHERE rl.ljubimac_id = l.id
+		) AS broj_rezervacija,
+		(
+			SELECT GROUP_CONCAT(rl.rezervacija_id ORDER BY rl.rezervacija_id ASC SEPARATOR ', ')
+				FROM rezervacija_ljubimac rl
+				WHERE rl.ljubimac_id = l.id
+		) AS id_rezervacija
+		FROM ljubimac l
+		JOIN pasmina p ON l.pasmina_id = p.id
+		JOIN vrsta v ON p.vrsta_id = v.id;
+
+SELECT * FROM detaljan_prikaz_ljubimac;
+
+-- 2. Pogled: vrsta statistika
+
+CREATE VIEW vrsta_statistika AS
+	SELECT 
+		v.id AS d,
+		v.naziv AS naziv,
+		AVG(l.dob) AS prosjecna_dob,
+		MIN(l.dob) AS minimalna_dob,
+		MAX(l.dob) AS maximalna_dob,
+		STDDEV(l.dob) AS standardna_devijacaija_dob,
+		SUM(l.dob) AS ukupna_dob,
+		AVG(l.tezina) AS prosjecna_tezina,
+		MIN(l.tezina) AS najmanja_tezina,
+		MAX(l.tezina) AS najveca_tezina,
+		STDDEV(l.tezina) AS standardna_devijacaija_tezina,
+		SUM(l.tezina) AS ukupna_tezina,
+		COUNT(l.id) AS broj_ljubimaca
+		FROM vrsta v
+		JOIN pasmina p ON v.id = p.vrsta_id
+		JOIN ljubimac l ON p.id = l.pasmina_id
+		GROUP BY v.id;
+
+SELECT * FROM vrsta_statistika;
+
+-- 3. Pogled: ljubimci s više rezervacija
+
+CREATE VIEW ljubimci_s_vise_rezervacija AS
+	SELECT 
+		l.id AS ljubimac_id,
+		l.ime AS ljubimac_ime,
+		COUNT(rl.rezervacija_id) AS broj_rezervacija
+		FROM ljubimac l
+		JOIN rezervacija_ljubimac rl ON l.id = rl.ljubimac_id
+		GROUP BY l.id
+		HAVING COUNT(rl.rezervacija_id) > 1;
+        
+DROP VIEW ljubimci_s_vise_rezervacija;
+        
+SELECT * FROM ljubimci_s_vise_rezervacija;
+
+-- 4. Pogled: Pasmina statistika
+
+CREATE VIEW pasmina_statistika AS
+	SELECT
+		p.id AS pasmina_id,
+		p.naziv AS pasmina_naziv,
+		p.opis AS pasmina_opis,
+		p.porijeklo AS pasmina_porijeklo,
+		p.tezina AS pasmina_tezina,
+		p.zivotni_vijek AS pasmina_zivotni_vijek,
+		v.naziv AS vrsta_naziv,
+		COUNT(l.id) AS broj_ljubimaca,
+		AVG(l.dob) AS prosjecna_dob_ljubimaca,
+		MIN(l.dob) AS najmanja_dob_ljubimaca,
+		MAX(l.dob) AS najveca_dob_ljubimaca,
+		AVG(l.tezina) AS prosjecna_tezina_ljubimaca,
+		MIN(l.tezina) AS najmanja_tezina_ljubimaca,
+		MAX(l.tezina) AS najveca_tezina_ljubimaca,
+		(SELECT AVG(rez_count)
+			FROM (
+				SELECT COUNT(rl.rezervacija_id) AS rez_count
+					FROM ljubimac l2
+					LEFT JOIN rezervacija_ljubimac rl ON l2.id = rl.ljubimac_id
+					WHERE l2.pasmina_id = p.id
+					GROUP BY l2.id
+		 ) AS broj_rez) AS prosjecan_broj_rezervacija_po_ljubimcu
+		FROM pasmina p
+		LEFT JOIN vrsta v ON p.vrsta_id = v.id
+		LEFT JOIN ljubimac l ON p.id = l.pasmina_id
+		GROUP BY p.id, v.naziv;
+
+SELECT * FROM pasmina_statistika;
+
